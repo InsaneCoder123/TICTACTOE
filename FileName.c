d7d95e678e57b9c1bdd1d6d47825ea4c3e6fce5f
InsaneCoder <kemdavid10@gmail.com>
2024-03-14 23:36:13 +0800

    Optimized

--- FileName.c
+++ FileName.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Remember to change the console clear command accordingly
 
 //TODO
 //Implement try again
@@ -84,7 +85,8 @@ void printUI(int board[9][3], int scoreboard[], int player, int* gameState) {
     //n => New Line
     //'>' => End Format
     int screenLength = 57, screenHeight = 20;
-    int boardDataPointer[6] = {0, 0, 0, 0, 0, 1}; //In order, dataPointerX{0}, dataPointerY{1}, screenPointerX{2}, screenPointerY{3}, formatPointer{4}, isEnabled{5}
+    int boardDataPointer[6] = {0, 0, 0, 0, 0, 1}; 
+    //In order, dataPointerX{0}, dataPointerY{1}, screenPointerX{2}, screenPointerY{3}, formatPointer{4}, isEnabled{5}
 
     int boardOrigin[2] = { 20, 5 };
     char boardFormat[] = "    +   +   +   +n"
@@ -98,12 +100,12 @@ void printUI(int board[9][3], int scoreboard[], int player, int* gameState) {
                         "+   +---+---+---+>";
 
     int playerTurnDataPointer[6] = { 0, 0, 0, 0, 0, 1 };
-    int playerTurnTextOrigin[2] = { 22, 16 };
-    char playerTurnText[] = "PLAYER # TURN>";
+    int playerTurnFormatOrigin[2] = { 22, 16 };
+    char playerTurnFormat[] = "PLAYER # TURN>";
     
     int playerMessageDataPointer[6] = { 0, 0, 0, 0, 0, 1 };
     int playerMessageOrigin[2] = { 14, 17 };
-    char playerMessageText[] = "ENTER COLUMN AND ROW (E.G, 0 1)>";
+    char playerMessageFormat[] = "ENTER COLUMN AND ROW (E.G, 0 1)>";
 
     int playerOneDataPointer[6] = { 0, 0, 0, 0, 0, 1 };
     int playerOneScoreOrigin[2] = { 5, 5 };
@@ -151,7 +153,7 @@ void printUI(int board[9][3], int scoreboard[], int player, int* gameState) {
 
 
     switch (*gameState) {
-    case 0:
+    case 0: //Config Screen; prompts user to enter the number of rounds to win
         boardDataPointer[5] = 0;
         playerMessageDataPointer[5] = 0;
         victoryDataPointer[5] = 0;
@@ -178,7 +180,7 @@ void printUI(int board[9][3], int scoreboard[], int player, int* gameState) {
         playerTwoScoreOrigin[0] = 30;
         playerTwoScoreOrigin[1] = 5;
         break;
-    case 3:
+    case 3: //Game Over Screen; displays a message and prompts user to play again
         boardDataPointer[5] = 0;
         playerMessageDataPointer[5] = 0;
         victoryDataPointer[5] = 0;
@@ -205,12 +207,12 @@ void printUI(int board[9][3], int scoreboard[], int player, int* gameState) {
                 printFormat(NULL, NULL, board, boardFormat, boardDataPointer);
                 continue;
             }
-            if (isOrigin(playerTurnTextOrigin, playerTurnDataPointer, x, y) == 1) {
-                printFormat(player, NULL, board, playerTurnText, playerTurnDataPointer);
+            if (isOrigin(playerTurnFormatOrigin, playerTurnDataPointer, x, y) == 1) {
+                printFormat(player, NULL, board, playerTurnFormat, playerTurnDataPointer);
                 continue;
             }
             if (isOrigin(playerMessageOrigin, playerMessageDataPointer, x, y) == 1) {
-                printFormat(NULL, NULL, board, playerMessageText, playerMessageDataPointer);
+                printFormat(NULL, NULL, board, playerMessageFormat, playerMessageDataPointer);
                 continue;
             }
             if (isOrigin(playerOneScoreOrigin, playerOneDataPointer, x, y) == 1) {
@@ -250,21 +252,24 @@ void printUI(int board[9][3], int scoreboard[], int player, int* gameState) {
 }
 
 //Resets the data of the board and the scoreboard
-void resetBoard(int board[9][3]) {
+void resetBoard(int board[9][3], int scoreboard[]) {
     for (int i = 0; i < 9; i++) {
 		board[i][2] = 0;
 	}
+    scoreboard[3] = 0;
+
 }
 
 //Resets the scores of the players, including the winner player index
 void resetScore(int scoreboard[]) {
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 4; i++) {
 		scoreboard[i] = 0;
 	}
 }
 
 //Update the data of a specific cell in the board
-void updateData(int board[9][3], int x, int y, int n) {
+void updateData(int board[9][3], int x, int y, int n, int scoreboard[]) {
+    scoreboard[3]++; //Increments the number of turns
     for (int k = 0; k < 9; k++) {
         if (board[k][0] == x && board[k][1] == y) {
             board[k][2] = n;
@@ -272,131 +277,38 @@ void updateData(int board[9][3], int x, int y, int n) {
         }
     }
 }
+
+
 //To be optimized
 int checkForWinner(int board[9][3], int scoreboard[]) {
-    //Horizontal
-    int playerOneCount = 0;
-    int playerTwoCount = 0;
-    for (int y = 0; y < 3; y++) {
-        for (int x = 0; x < 3; x++) {
-            for (int i = 0; i < 9; i++) {
-                if (board[i][0] == x && board[i][1] == y) {
-                    if (board[i][2] == 1) {
-                        playerOneCount++;
-                        if (playerOneCount >= 3) {
-                            scoreboard[0]++;
-                            return 1;
-                        }
-                    }
-                    else if (board[i][2] == 2) {
-                        playerTwoCount++;
-                        if (playerTwoCount >= 3) {
-                            scoreboard[1]++;
-                            return 2;
-                        }
-                    }
-                }
+    for (int n = 0; n < 3; n++) {
+        for (int i = 1; i < 3; i++) {
+            //Horizontal
+            if (board[findIndex(board, 0, n)][2] == i && board[findIndex(board, 1, n)][2] == i && board[findIndex(board, 2, n)][2] == i) {
+                scoreboard[i - 1]++;
+                return i;
             }
-        }
-        playerOneCount = 0;
-        playerTwoCount = 0;
-    }
-
-    //Vertical
-    for (int x = 0; x < 3; x++) {
-        for (int y = 0; y < 3; y++) {
-            for (int i = 0; i < 9; i++) {
-                if (board[i][0] == x && board[i][1] == y) {
-                    if (board[i][2] == 1) {
-                        playerOneCount++;
-                        if (playerOneCount >= 3) {
-                            scoreboard[0]++;
-                            return 1;
-                        }
-                    }
-                    else if (board[i][2] == 2) {
-                        playerTwoCount++;
-                        if (playerTwoCount >= 3) {
-                            scoreboard[1]++;
-                            return 2;
-                        }
-                    }
-                }
+            //Vertical
+            if (board[findIndex(board, n, 0)][2] == i && board[findIndex(board, n, 1)][2] == i && board[findIndex(board, n, 2)][2] == i) {
+                scoreboard[i - 1]++;
+                return i;
             }
-        }
-        playerOneCount = 0;
-        playerTwoCount = 0;
-    }
-
-    //Diagonal
-    for (int n = 0; n < 9; n++) {
-        for (int i = 0; i < 3; i++) {
-            if (board[n][0] == i && board[n][1] == i) {
-                if (board[n][2] == 1) {
-                    playerOneCount += 1;
-                    if (playerOneCount >= 3) {
-                        scoreboard[0]++;
-                        return 1;
-                    }
-                }
-                else if (board[n][2] == 2) {
-                    playerTwoCount += 1;
-                    if (playerTwoCount >= 3) {
-                        scoreboard[1]++;
-                        return 2;
-                    }
-                }
+            //Diagonal
+            if (board[findIndex(board, 0, 0)][2] == i && board[findIndex(board, 1, 1)][2] == i && board[findIndex(board, 2, 2)][2] == i) {
+                scoreboard[i - 1]++;
+                return i;
+            }
+            //Diagonal
+            if (board[findIndex(board, 0, 2)][2] == i && board[findIndex(board, 1, 1)][2] == i && board[findIndex(board, 2, 0)][2] == i) {
+                scoreboard[i - 1]++;
+                return i;
+            }
+            //Draw; if the number of turns is equal to the number of cells in the board
+            if (scoreboard[3] >= 9) {
+                return 3;
             }
         }
     }
-    playerOneCount = 0;
-    playerTwoCount = 0;
-
-    int count = 0;
-    for (int x = 2; x >= 0; x--) {
-        for (int i = 0; i < 9; i++) {
-            if (board[i][0] == x && board[i][1] == count) {
-                if (board[i][2] == 1) {
-                    playerOneCount += 1;
-                    if (playerOneCount >= 3) {
-                        scoreboard[0]++;
-                        return 1;
-                    }
-                }
-                else if (board[i][2] == 2) {
-                    playerTwoCount += 1;
-                    if (playerTwoCount >= 3) {
-                        scoreboard[1]++;
-                        return 2;
-                    }
-                }
-            }
-        }
-        count++;
-    }
-
-    //Draw
-    for (int j = 0; j < 3; j++) {
-        for (int i = 0; i < 3; i++) {
-            for (int n = 0; n < 9; n++) {
-				if (board[n][0] == i && board[n][1] == j) {
-					if (board[n][2] == 1 || board[n][2] == 2) {
-						if (n == 8) {
-                            scoreboard[0]++;
-                            scoreboard[1]++;
-							return 3;
-						}
-					}
-					else {
-						return 0;
-					}
-				}
-			}
-        }
-    }
-
-
-
     return 0;
 }
 void playerInput(int board[9][3], int scoreboard[], int player, int* gameState){
@@ -430,7 +342,7 @@ void playerInput(int board[9][3], int scoreboard[], int player, int* gameState){
             printf("{Invalid Input: Please enter a coordinate with a free space}");
         }
     }
-    updateData(board, playerInput[0]-1, playerInput[1]-1, player);
+    updateData(board, playerInput[0]-1, playerInput[1]-1, player, scoreboard);
 }
 
 int main()
@@ -441,68 +353,70 @@ int main()
     int winningScore = 0;
     int valid = 0;
     char optionTry = ' ';
-    int board[][3] = { {0, 0, 1}, {1, 0, 1}, {2, 0, 0},
+    int board[][3] = { {0, 0, 0}, {1, 0, 0}, {2, 0, 0},
                     {0, 1, 0}, {1, 1, 0}, {2, 1, 0},
                     {0, 2, 0}, {1, 2, 0}, {2, 2, 0} };
-    int scoreboard[] = {0, 0, 0};
+    int scoreboard[] = {0, 0, 0, 0}; //Player 1 score, Player 2 score, Winner, Turns
     int isWinner = 0;
     while (isRunning == 0) {
         valid = 0;
-        if (gameState == 0) {
-            printUI(board, scoreboard, 0, &gameState);
-            while (valid == 0) {
-                scanf("%d", &winningScore);
-                if (winningScore > 0) {
-                    valid = 1;
-                    system("cls");
-                    gameState = 1;
+        switch (gameState) {
+            case 0:
+                printUI(board, scoreboard, 0, &gameState);
+                while (valid == 0) {
+                    scanf("%d", &winningScore);
+                    if (winningScore > 0) {
+                        valid = 1;
+                        gameState = 1;
+                        system("cls");
+                    }
+                    else {
+                        while ((getchar()) != '\n'); //Clears input buffer
+                        system("cls");
+                        printUI(board, scoreboard, 0, &gameState);
+                        printf("{Invalid Input: Please enter a number greater than 0}\n");
+                    }
                 }
-                else {
+                break;
+            case 1:
+                for (int i = 1; i < 3; i++) {
+                    printUI(board, scoreboard, i, &gameState);
+                    playerInput(board, scoreboard, i, &gameState);
+                    system("cls");
+                    isWinner = checkForWinner(board, scoreboard);
+                    if (isWinner != 0) {
+                        resetBoard(board, scoreboard);
+                        if (isWinner == 1) { i = 1; }
+                        else if (isWinner == 2) { i = 0; }
+                        if (scoreboard[i - 1] >= winningScore) { gameState = 2; scoreboard[2] = i; break; }
+                    }
+                }
+                break;
+            case 2:
+                printUI(board, scoreboard, 0, &gameState);
+                while (valid == 0) {
                     while ((getchar()) != '\n'); //Clears input buffer
-                    system("cls");
-					printUI(board, scoreboard, 0, &gameState);
-					printf("{Invalid Input: Please enter a number greater than 0}\n");
-                }
-            }
-
-        }
-        else if (gameState == 1) {
-            for (int i = 1; i < 3; i++) {
-                printUI(board, scoreboard, i, &gameState);
-                playerInput(board, scoreboard, i, &gameState);
-                system("cls"); //Remember to change the console clear command according to the compiler
-                isWinner = checkForWinner(board, scoreboard);
-                if (isWinner != 0) {
-                    resetBoard(board);
-                    i = 1;
-                    if (scoreboard[i - 1] >= winningScore) { gameState = 2; scoreboard[2] = i; break; }
-                }
-            }
-        }
-        else if (gameState == 2) {
-            printUI(board, scoreboard, 0, &gameState);
-            while (valid == 0) {
-                while ((getchar()) != '\n'); //Clears input buffer
-                scanf("%c", &optionTry);
-                switch (optionTry) {
+                    scanf("%c", &optionTry);
+                    switch (optionTry) {
                     case 'Y':
-						valid = 1;
-						gameState = 1;
+                        valid = 1;
+                        gameState = 1;
                         resetScore(scoreboard);
                         system("cls");
-						break;
+                        break;
                     case 'N':
                         system("cls");
                         gameState = 3;
                         printUI(board, scoreboard, 0, &gameState);
                         return;
                     default:
-                        system("cls");    
+                        system("cls");
                         printUI(board, scoreboard, 0, &gameState);
                         printf("{Invalid Input: Please enter either Y or N}\n");
-						break;
+                        break;
+                    }
                 }
-            }
+                break;
         }
     }
     return 0;
